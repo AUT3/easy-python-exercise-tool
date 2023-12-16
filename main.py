06024c3a492e31d141291ee2d5bb69cac5692d1b
AUT3 <paul.wozniak.official@gmail.com>
2023-12-16 23:50:22 +0100

    Broken up the program into two to make it easier to make it portable as an exe in the future

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import json
+import program
+import argparse
+import configparser
 
-exercise_path = "example.json" 
+# file mainly for handling arguments
+
+exercise_path: str
+solution_path: str
+portable = False
 
 if __name__ == "__main__":
-    exercise_file = open(exercise_path, "r")
-    print(json.loads(exercise_file.read()))
+    # parsing arguments: src, (-F, -o)
+    parser = argparse.ArgumentParser(description="Software to make creating Python exercises easier")
+
+    parser.add_argument("-f", "--file", type=str, help="Specify the solution file (if applicable)", default="")
+    parser.add_argument("-p", "--portable", action="store_true", help="Output to .exe file named after the exercise file (for easier portability)")
+    parser.add_argument("exercise-file", type=str, help="Specify the exercise (json) file")
     
+    args = parser.parse_args()
+    arg_config = vars(args)
+
+    # print(config)
+    exercise_path = arg_config["exercise-file"]
+    solution_path = arg_config["file"]
+    portable = arg_config["portable"]
+
+
+    exe = program.exercise(exercise_path, solution_path)
+    program.loop(exe)
---
+++ program.py
@@ -0,0 +1,80 @@
+import json
+import subprocess
+import configparser
+
+# file with the actual program
+
+class exercise:
+    def __init__(self, path, solution_path) -> None:
+        self.path = path
+        file = open(path, "r")
+        self.ex = json.loads(file.read())
+        file.close()
+        self.clear_file = True
+        self.solution_path = solution_path
+
+    def handle_solution_file(self, path):
+        # create or clear file (works, weirdly enough)
+        if(self.clear_file):
+            open(path, "w").close()
+
+        input(f"Write your answer in {path} and press Enter when done\n>")
+
+        ans_file = open(path, "r")
+        ans = ans_file.read()
+        ans_file.close()
+
+        return ans
+
+    def show_question(self, num) -> bool:
+        q = self.ex[num]
+        output = False
+
+        if(num != 0):
+            print("\n")
+
+        print(q["title"])
+        print(q["question"])
+        
+        if(q["type"] == "sc" or q["type"] == "mc"):
+            # single-choice / multiple-choice (same thing code wise, might merge them in json to "c" for choice)
+            ans = input("Type your answer here:\n>").lower()
+
+            # TODO: regex here
+
+            output = (ans == q["answer"])
+        elif(q["type"]=="e"):
+            # executable exercise (placeholder name) - requires code to be written
+
+            if(len(q.get('file', '')) == 0):
+                if(len(self.solution_path) == 0):
+                    ans = input("Type your code here:\n>")
+                else:
+                    ans = self.handle_solution_file(self.solution_path)
+            else:
+                ans = self.handle_solution_file(q["file"])
+
+            result = subprocess.run(["python3", "-c", ans], capture_output=True).stdout
+            expected = subprocess.run(["python3", "-c", q["answer"]], capture_output=True).stdout
+
+            print(f"Your result: {result}\nExpected result: {expected}")
+            
+            output = (result == expected)
+        else:
+            print(f"Error! Probably wrong type in question {num}!!!")
+            output = False
+
+        self.clear_file = output
+
+        return output
+
+def loop(e: exercise):
+    i = 0
+    while(i < len(e.ex)):
+        if(e.show_question(i)):
+            i += 1
+
+    print("Congratulations!")
+
+if __name__ == "__main__":
+    pass
